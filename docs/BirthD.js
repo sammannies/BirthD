6b85963c77d8e4561386aaae12f54ffbf878ddfe
sammannies <40059448+sammannies@users.noreply.github.com>
2018-06-08 12:53:18 +0200

    Update BirthD.js

--- docs/BirthD.js
+++ docs/BirthD.js
@@ -1,7 +1,3 @@
 "use strict";
 
-$("#header").load('header.html');
-$("#footer").load('footer.html');
-
-// Global variables used by our Dapp
 var contract_address = "n1rvbpkXJmfRTT2bZoWJwcY6qL1iiDgKN17";
